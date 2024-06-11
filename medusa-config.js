d3896c4dd17b4b8bc4e6fce7f8d6fab9c0e33fdb
Bruno Silva <bruno.m.silva@icloud.com>
2024-06-11 19:29:13 +0100

    s

--- medusa-config.js
+++ medusa-config.js
@@ -113,13 +113,13 @@ const projectConfig = {
   admin_cors: ADMIN_CORS,
   redis_url: REDIS_URL,
   worker_mode: process.env.MEDUSA_WORKER_MODE,
-  // database_extra: process.env.NODE_ENV !== "development" ? { ssl: { rejectUnauthorized: false } } : {},
+  database_extra: process.env.NODE_ENV !== "development" ? { ssl: { rejectUnauthorized: false } } : {},
   database_driver_options: process.env.NODE_ENV !== "development" ? { connection: { ssl: { rejectUnauthorized: false } } } : {}
  };
 
  console.log(projectConfig.database_extra)
  console.log(projectConfig.database_driver_options)
- console.log(process.env.NODE_ENV)
+ console.log(projectConfig)
 
 
 /** @type {import('@medusajs/medusa').ConfigModule} */
