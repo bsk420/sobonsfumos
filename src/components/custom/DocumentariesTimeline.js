bf3623b29756cddb60d5eea17aa02f8d219dff19
Bruno Silva <bruno.m.silva@icloud.com>
2024-07-10 23:29:31 +0100

    fix

--- src/components/custom/DocumentariesTimeline.js
+++ src/components/custom/DocumentariesTimeline.js
@@ -23,10 +23,6 @@ const Image = styled.div(props => [
   `background-image: url("${props.imageSrc}");`,
   tw`rounded md:w-1/2 lg:w-5/12 xl:w-1/3 flex-shrink-0 h-80 md:h-144 bg-cover bg-center mx-4 sm:mx-8 md:mx-4 lg:mx-8`
 ]);
-const ImageLink = styled.a(props => [
-  <Image imageSrc={props.imageSrc} />,
-  tw`rounded md:w-1/2 lg:w-5/12 xl:w-1/3 flex-shrink-0 h-80 md:h-144 bg-cover bg-center mx-4 sm:mx-8 md:mx-4 lg:mx-8`
-]);
 const Details = tw.div`mt-4 md:mt-0 md:max-w-md mx-4 sm:mx-8 md:mx-4 lg:mx-8`;
 const Subtitle = tw.div`font-bold tracking-wide text-secondary-100`;
 const Title = tw.h4`text-3xl font-bold text-white`;
