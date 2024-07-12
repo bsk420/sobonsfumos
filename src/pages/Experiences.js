12be3ed93b5dbc9becde2fb44a4b45c3b12f1a2f
Bruno Silva <bruno.m.silva@icloud.com>
2024-07-12 10:44:18 +0100

    x

--- src/pages/Experiences.js
+++ src/pages/Experiences.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRouter} from "react";
 import AnimationRevealPage from "helpers/AnimationRevealPage.js";
 import tw from "twin.macro";
 
