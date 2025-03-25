296293fc0479300d8569e5ad559eb1734a07203c
Bruno Silva <98500231+bsk420@users.noreply.github.com>
2025-03-25 09:34:33 +0000

    Update ExperiencesTimeline.js

--- src/components/custom/ExperiencesTimeline.js
+++ src/components/custom/ExperiencesTimeline.js
@@ -68,7 +68,7 @@ export default () => {
         <Content>
           {cards.map((card, i) => (
             <Card key={i} reversed={i % 2 === 1}>
-              <Image imageSrc={card.imageSrc} />
+              <Image imageSrc={card.imageSrc} style={{ object-fit: contain; }} />
               <Details>
                 <Subtitle>{card.subtitle}</Subtitle>
                 <Title>{card.title}</Title>
